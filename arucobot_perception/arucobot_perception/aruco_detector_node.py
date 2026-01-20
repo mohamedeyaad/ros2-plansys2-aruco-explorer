fe27a25aa5f4ca3f2b8ebb1bc6153380900571fc
Mohamed Eyad <mooeyad@gmail.com>
2026-01-21 00:33:15 +0100

    fix: update import path for ArucoMarkers from aruco_interfaces to arucobot_interfaces

--- arucobot_perception/arucobot_perception/aruco_detector_node.py
+++ arucobot_perception/arucobot_perception/aruco_detector_node.py
@@ -7,7 +7,7 @@ import numpy as np
 
 from sensor_msgs.msg import CameraInfo, Image
 from geometry_msgs.msg import Pose
-from aruco_interfaces.msg import ArucoMarkers
+from arucobot_interfaces.msg import ArucoMarkers
 from rcl_interfaces.msg import ParameterDescriptor, ParameterType
 
 class ArucoNode(Node):
