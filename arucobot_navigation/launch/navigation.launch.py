174c8b4caf6c2cb2d757a64c61c9dce33e7c9358
Mohamed Eyad <mooeyad@gmail.com>
2026-01-20 01:10:23 +0100

    feat: add RViz configuration and update install directory for navigation resources

--- arucobot_navigation/launch/navigation.launch.py
+++ arucobot_navigation/launch/navigation.launch.py
@@ -3,21 +3,22 @@ from ament_index_python.packages import get_package_share_directory
 from launch import LaunchDescription
 from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
-from launch.substitutions import LaunchConfiguration
 from launch_ros.actions import Node
 
 def generate_launch_description():
-    pkg_name = 'arucobot_navigation'
-    pkg_share = get_package_share_directory(pkg_name)
+
+    # Get the package directories
+    pkg_arucobot_navigation = get_package_share_directory('arucobot_navigation')
+    pkg_nav2_bringup = get_package_share_directory('nav2_bringup')
+    pkg_arucobot_gazebo = get_package_share_directory('arucobot_gazebo')
     
     # Files
-    nav2_params_file = os.path.join(pkg_share, 'config', 'nav2_params.yaml')
-    map_file = os.path.join(pkg_share, 'maps', 'my_map.yaml') # It should matche what the name of the map!
-
+    nav2_params_file = os.path.join(pkg_arucobot_navigation, 'config', 'nav2_params.yaml')
+    map_file = os.path.join(pkg_arucobot_navigation, 'maps', 'my_map.yaml') # It should matche what the name of the map!
     # 1. Launch the Simulation & Bridge (existing launch file)
     sim_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('arucobot_gazebo'), 'launch', 'simulation.launch.py')
+            os.path.join(pkg_arucobot_gazebo, 'launch', 'simulation.launch.py')
         ),
         launch_arguments={
             'use_rviz': 'False'  # Disable RViz in the simulation launch
@@ -27,7 +28,7 @@ def generate_launch_description():
     # 2. Launch Nav2 Bringup (Localization + Path Planning)
     nav2_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
-            os.path.join(get_package_share_directory('nav2_bringup'), 'launch', 'bringup_launch.py')
+            os.path.join(pkg_nav2_bringup, 'launch', 'bringup_launch.py')
         ),
         launch_arguments={
             'use_sim_time': 'True',
@@ -37,8 +38,7 @@ def generate_launch_description():
     )
 
     # 3. Launch RViz (Pre-configured for Nav2)
-    # Use the official Nav2 rviz config so no need to setup displays manually
-    rviz_config = os.path.join(get_package_share_directory('nav2_bringup'), 'rviz', 'nav2_default_view.rviz')
+    rviz_config = os.path.join(pkg_arucobot_navigation, 'rviz', 'arucobot_navigation.rviz')
     rviz_node = Node(
         package='rviz2',
         executable='rviz2',
